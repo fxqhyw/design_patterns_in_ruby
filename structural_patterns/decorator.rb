14da121bcc54c945d50c6cd6b4e9a901f522a85e
Sergey Kozhemiako <therealloker@gmail.com>
2019-12-26 15:07:49 +0200

    Decorator implementation update

--- structural_patterns/decorator.rb
+++ structural_patterns/decorator.rb
@@ -1,47 +1,80 @@
-# frozen_string_literal: true
-
-# This pattern is designed to dynamically connect additional behavior to the object.
-
-class Robot
-  attr_reader :model_name, :id, :release_date
-
-  def initialize(model_name:, id:, release_date:)
-    @model_name = model_name
-    @id = id
-    @release_date = release_date
+# The base Component interface defines operations that can be altered by
+# decorators.
+class Component
+  # @return [String]
+  def operation
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
 
-class RobotFirstDecorator < SimpleDelegator
-  def age
-    ((Time.now - release_date) / 315_576_00).floor
+# Concrete Components provide default implementations of the operations. There
+# might be several variations of these classes.
+class ConcreteComponent < Component
+  # @return [String]
+  def operation
+    'ConcreteComponent'
   end
 end
 
-class RobotSecondDecorator < SimpleDelegator
-  def full_name
-    "#{id} => #{model_name}"
+# The base Decorator class follows the same interface as the other components.
+# The primary purpose of this class is to define the wrapping interface for all
+# concrete decorators. The default implementation of the wrapping code might
+# include a field for storing a wrapped component and the means to initialize
+# it.
+class Decorator < Component
+  attr_accessor :component
+
+  # @param [Component] component
+  def initialize(component)
+    @component = component
+  end
+
+  # The Decorator delegates all work to the wrapped component.
+  def operation
+    @component.operation
   end
 end
 
-robot = Robot.new(
-  release_date: Time.new(2015, 3, 2, 3, 0, '+03:00'),
-  id: '934kawrr22',
-  model_name: 'IOI'
-)
+# Concrete Decorators call the wrapped object and alter its result in some way.
+class ConcreteDecoratorA < Decorator
+  # Decorators may call parent implementation of the operation, instead of
+  # calling the wrapped object directly. This approach simplifies extension of
+  # decorator classes.
+  def operation
+    "ConcreteDecoratorA(#{@component.operation})"
+  end
+end
 
-robot_decorator = RobotFirstDecorator.new(robot)
-robot_decorator = RobotSecondDecorator.new(robot_decorator)
+# Decorators can execute their behavior either before or after the call to a
+# wrapped object.
+class ConcreteDecoratorB < Decorator
+  # @return [String]
+  def operation
+    "ConcreteDecoratorB(#{@component.operation})"
+  end
+end
 
-puts robot_decorator.class
-# RobotSecondDecorator
-puts robot_decorator.id
-# 934kawrr22
-puts robot_decorator.model_name
-# IOI
-puts robot_decorator.full_name
-# 934kawrr22 => IOI
-puts robot_decorator.release_date
-# 2015-03-02 03:00:03 +0300
-puts robot_decorator.age
-# 2
+# The client code works with all objects using the Component interface. This way
+# it can stay independent of the concrete classes of components it works with.
+def client_code(component)
+  # ...
+
+  print "RESULT: #{component.operation}"
+
+  # ...
+end
+
+# This way the client code can support both simple components...
+simple = ConcreteComponent.new
+puts 'Client: I\'ve got a simple component:'
+client_code(simple)
+puts "\n\n"
+
+# ...as well as decorated ones.
+#
+# Note how decorators can wrap not only simple components but the other
+# decorators as well.
+decorator1 = ConcreteDecoratorA.new(simple)
+decorator2 = ConcreteDecoratorB.new(decorator1)
+puts 'Client: Now I\'ve got a decorated component:'
+client_code(decorator2)
